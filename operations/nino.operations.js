a61512665a2f217fcb36bdd5966f0614ed84eb0f
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-11 00:01:37 -0500

    other operations implementation

--- operations/nino.operations.js
+++ operations/nino.operations.js
@@ -5,9 +5,33 @@ ninoOperations.getNinos = async function(req, res) {
 	const ninos = await coleccionNinos.find();
 	res.json(ninos);
 }
-ninoOperations.getNino = function(req, res) {}
-ninoOperations.crearNino = function(req, res) {}
-ninoOperations.actualizarNino = function(req, res) {}
+
+ninoOperations.getNino = async function(req, res) {
+	const nino = await coleccionNinos.findById(req.params.id);
+	res.json(nino);
+}
+
+ninoOperations.crearNino = async function(req, res) {
+	const nino = new coleccionNinos(req.body);
+	await nino.save();
+	res.json({"status":"Dato de niño guardado"});
+}
+
+ninoOperations.actualizarNino = async function(req, res) {
+	const { id } = req.params;
+	const nino = {
+		nombres: req.body.nombres,
+    	apellidos: req.body.apellidos,
+    	documento:{
+        	tipo: req.body.documento.tipo,
+        	numero: req.body.documento.numero
+    	}
+	}
+	console.log(nino)
+	await coleccionNinos.findByIdAndUpdate(req.params.id, {$set: nino}, {new: true});
+	res.json({"status":"Dato de niño actualizado"});
+}
+
 ninoOperations.borrarNino = function(req, res) {}
 
 module.exports = ninoOperations
